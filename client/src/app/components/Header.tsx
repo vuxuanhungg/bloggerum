3eec94eb1a9c6044bd010bfd8d11b4dc8e577df5
xuanhung1509 <vuxuanhung15092002@gmail.com>
2024-04-08 10:26:48 +0700

    feat(client): add logout functionality

--- client/src/app/components/Header.tsx
+++ client/src/app/components/Header.tsx
@@ -1,9 +1,26 @@
 'use client'
 import Link from 'next/link'
 import { useAuthContext } from '../context/AuthContext'
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-toastify'
 
 const Header = () => {
-    const { isLoading, user } = useAuthContext()
+    const router = useRouter()
+    const { isLoading, user, setUser } = useAuthContext()
+
+    const logOut = async () => {
+        const res = await fetch('http://localhost:8080/api/users/logout', {
+            method: 'POST',
+            credentials: 'include',
+        })
+        if (res.ok) {
+            setUser(null)
+            toast.success('Successfully logged out!')
+            router.push('/login')
+            return
+        }
+        toast.error('Error occurred')
+    }
 
     return (
         <header className="container my-8 flex items-center justify-between gap-4">
@@ -34,13 +51,21 @@ const Header = () => {
                         </ul>
                     )}
                     {user && (
-                        <Link
-                            href={`/user/${user._id}`}
-                            className="flex items-center gap-2"
-                        >
-                            <div className="h-8 w-8 rounded-full bg-slate-500"></div>
-                            {user?.name}
-                        </Link>
+                        <div className="flex items-center gap-3">
+                            <Link
+                                href={`/user/${user._id}`}
+                                className="flex items-center gap-2"
+                            >
+                                <div className="h-8 w-8 rounded-full bg-slate-500"></div>
+                                {user?.name}
+                            </Link>
+                            <button
+                                className="rounded border border-slate-300 px-5 py-2"
+                                onClick={logOut}
+                            >
+                                Log Out
+                            </button>
+                        </div>
                     )}
                 </nav>
             )}
